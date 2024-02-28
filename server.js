67e9a125e954cd156c83547cce6c35548c5fd425
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-02-28 17:45:12 +0700

    .

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+
+// Khởi tạo server BE chạy port 8080
+app.listen(8080);
+
+// Khởi động server BE bằng lệnh node server.js
+
+// GET url: localhost:8080/demo
+// endpoint /demo
+
+app.get();
+app.post();
+app.put();
+app.delete();
