bddb4ca500b2f45d88255e2ab2eb2b45c1c9538e
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-02-28 17:59:53 +0700

    .

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ app.listen(8080);
 // GET url: localhost:8080/demo
 // endpoint /demo
 
-app.get();
-app.post();
-app.put();
-app.delete();
+// tham số 1: tên endpoint
+// tham số 2: arrow function
+app.get("/demo/:id2/:email2", (request, response) => {
+  // C1: Lấy từ URL
+  // -query string: /demo?id=123&email=demo@gmail.com
+  // -query params: /demo/123/demo@gmail.com
+  // C2: Lấy từ Json
+
+  // BE trả dữ liệu về cho FE
+  response.send("Hello world!!");
+  // Trả về tất cả dạng dữ liệu trừ number
+});
+// app.post();
+// app.put();
+// app.delete();
