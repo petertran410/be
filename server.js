7e4d8434d2530640a2cc2299c4dc3e4e33bbfd39
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-02-28 20:13:09 +0700

    .

--- server.js
+++ server.js
@@ -7,18 +7,31 @@ app.listen(8080);
 // Khởi động server BE bằng lệnh node server.js
 
 // GET url: localhost:8080/demo
-// endpoint /demo
+// endpoint /demo/:id2/:email2
 
 // tham số 1: tên endpoint
 // tham số 2: arrow function
-app.get("/demo/:id2/:email2", (request, response) => {
+app.get("/demo", (req, res) => {
   // C1: Lấy từ URL
   // -query string: /demo?id=123&email=demo@gmail.com
   // -query params: /demo/123/demo@gmail.com
+
+  // query string
+  // let id = req.query.id;
+  // let email = req.query.email;
+  // let { id, email } = req.query;
+
+  // query params
+  // let id2 = req.params.id2;
+  // let email2 = req.params.email2;
+  // let { id2, email2 } = req.params;
+
   // C2: Lấy từ Json
+  let { id, userName, email, phone, sex } = req.body;
 
   // BE trả dữ liệu về cho FE
-  response.send("Hello world!!");
+  // res.send({ id, email, id2, email2 });
+  res.send({ id, userName, email, phone, sex });
   // Trả về tất cả dạng dữ liệu trừ number
 });
 // app.post();
