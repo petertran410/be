4eda79a057ed0cfa37ca9fb15bd08a3bcef2bf6e
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-03-13 19:48:12 +0700

    Done Buoi 5

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,10 @@
-import Video from "../models/video.js";
+// import Video from "../models/video.js";
+import sequelize from "../models/connect.js";
+import initModels from "../models/init-models.js";
+import { Sequelize } from "sequelize";
+
+let model = initModels(sequelize);
+let Op = Sequelize.Op;
 
 // Quản lí, thực hiện chức năng
 export const getVideo = async (req, res) => {
@@ -7,9 +13,34 @@ export const getVideo = async (req, res) => {
 
     // SELECT * FROM video
     // let data = await Video.findAll();
+    // let data = await model.video.findAll();
 
-    // SELECT * FROM video WHERE
-    let data = await Video.findAll();
+    // SELECT * FROM video WHERE video_id = 2
+    // let data = await model.video.findAll({
+    //   where: {
+    //     video_id: 2,
+    //   },
+    // });
+
+    // SELECT * FROM video WHERE video_name LIKE "%code%"
+    // let data = await model.video.findAll({
+    //   where: {
+    //     video_name: {
+    //       [Op.like]: "%gaming%",
+    //     },
+    //   },
+    // });
+
+    // SELECT video_id, video_name FROM video WHERE video_name LIKE "%code%"
+    let data = await model.video.findAll({
+      where: {
+        video_name: {
+          [Op.like]: "%gaming%",
+        },
+      },
+      attributes: ["video_id", "video_name"],
+    });
+
     res.status(200).send(data);
   } catch (error) {
     res.status(500).send("Lỗi ...");
